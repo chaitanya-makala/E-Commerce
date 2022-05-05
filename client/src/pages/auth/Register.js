2d227bc3128db29a17f91005a7f9a2f69f9fe62a
Chaitanya Makala <chaitanya.m.1996@gmail.com>
2022-05-06 02:51:09 +0530

    add email registration to register.js

--- client/src/pages/auth/Register.js
+++ client/src/pages/auth/Register.js
@@ -1,9 +1,30 @@
 import React, { useState } from "react";
+import { auth } from "../../firebase";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Register = () => {
   const [email, setEmail] = useState("");
-  const handleSubmit = () => {
-    //
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const config = {
+      url: "http://localhost:3000/register/complete",
+      handleCodeInApp: true,
+    };
+
+    //send registration mail
+    await auth.sendSignInLinkToEmail(email, config);
+
+    //display toast notification
+    toast.success(
+      `Email is sent to ${email}. Click the link to complete your registration.`
+    );
+
+    //save email in local storage
+    window.localStorage.setItem("emailForRegistration", email);
+
+    //clear email state
+    setEmail("");
   };
   const handleChange = (event) => {
     setEmail(event.target.value);
@@ -30,6 +51,7 @@ const Register = () => {
       <div className="row">
         <div className="col-md-6 offset-md-3">
           <h4>Register</h4>
+          <ToastContainer />
           {registerForm()}
         </div>
       </div>
