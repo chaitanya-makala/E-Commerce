c15144b124d132f6f8ccc38a52723dd0ee1b00b7
Chaitanya Makala <chaitanya.m.1996@gmail.com>
2022-05-06 02:27:50 +0530

    create register form

--- client/src/pages/auth/Register.js
+++ client/src/pages/auth/Register.js
@@ -1,12 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Register = () => {
+  const [email, setEmail] = useState("");
+  const handleSubmit = () => {
+    //
+  };
+  const handleChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const registerForm = () => {
+    return (
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          className="form-control"
+          value={email}
+          onChange={handleChange}
+          autoFocus
+        />
+        <button type="submit" className="btn btn-raised">
+          Register
+        </button>
+      </form>
+    );
+  };
   return (
-    <>
-      <div>
-        <p>Register</p>
+    <div className="container p-5">
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <h4>Register</h4>
+          {registerForm()}
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
