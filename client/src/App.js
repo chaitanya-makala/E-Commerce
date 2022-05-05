ef3c67a564ae2f9abda731859761bef216f609ad
Chaitanya Makala <chaitanya.m.1996@gmail.com>
2022-05-06 01:00:00 +0530

    install react router dom, create pages and routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,17 @@
+import React from "react";
+import Home from "./pages/Home";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import { Route, Switch } from "react-router-dom";
+
 function App() {
-  return <></>;
+  return (
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/register" component={Register} />
+    </Switch>
+  );
 }
 
 export default App;
